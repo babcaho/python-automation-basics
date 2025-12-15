6547c72e137da8d7eb2493a59ac42e706f04f738
babcaho <babcaho@gmail.com>
2025-12-15 17:15:21 -0300

    organizador.py

--- 01_automacao_de_arquivos/organizador.py
+++ 01_automacao_de_arquivos/organizador.py
@@ -1,16 +1,13 @@
 import os
 
-arquivos = os.listdir()
-extensoes = {}
+def analisar_arquivos():
+    arquivos = os.listdir()
+    extensoes = {}
 
-for arquivo in arquivos:
-    if "." in arquivo:
-        ext = arquivo.split(".")[-1]
-        extensoes[ext] = extensoes.get(ext, 0) + 1
+    for arquivo in arquivos:
+        if "." in arquivo:
+            ext = arquivo.split(".")[-1]
+            extensoes[ext] = extensoes.get(ext, 0) + 1
 
-with open("relatorio.txt", "w") as relatorio:
-    relatorio.write("Relatório de Arquivos\n\n")
-    for ext, qtd in extensoes.items():
-        relatorio.write(f".{ext}: {qtd} arquivo(s)\n")
+    return extensoes
 
-print("Relatório criado com sucesso.")

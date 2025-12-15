149cebf7b813f32e15a87d4842c834b8e4aef5a7
babcaho <babcaho@gmail.com>
2025-12-15 16:40:31 -0300

    Projeto 1 - Automação de arquivos com relatório

--- 01_automacao_de_arquivos/organizador.py
+++ 01_automacao_de_arquivos/organizador.py
@@ -1,7 +1,16 @@
 import os
 
-print("Programa iniciado")
-
 arquivos = os.listdir()
+extensoes = {}
 
-print("Arquivos encontrados:", arquivos)
+for arquivo in arquivos:
+    if "." in arquivo:
+        ext = arquivo.split(".")[-1]
+        extensoes[ext] = extensoes.get(ext, 0) + 1
+
+with open("relatorio.txt", "w") as relatorio:
+    relatorio.write("Relatório de Arquivos\n\n")
+    for ext, qtd in extensoes.items():
+        relatorio.write(f".{ext}: {qtd} arquivo(s)\n")
+
+print("Relatório criado com sucesso.")

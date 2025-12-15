9d53decad4cd37671f8e98017d7f03939246c5f4
babcaho <babcaho@gmail.com>
2025-12-15 17:17:50 -0300

    organizador.py

--- 01_automacao_de_arquivos/organizador.py
+++ 01_automacao_de_arquivos/organizador.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 def analisar_arquivos():
     arquivos = os.listdir()
@@ -10,9 +11,25 @@ def analisar_arquivos():
             extensoes[ext] = extensoes.get(ext, 0) + 1
 
     return extensoes
-resultado = analisar_arquivos()
 
-print("Resumo dos arquivos:")
-for ext, qtd in resultado.items():
-    print(f".{ext}: {qtd} arquivo(s)")
+def gerar_relatorio(resultado):
+    data = datetime.now().strftime("%d/%m/%Y %H:%M")
+
+    with open("relatorio.txt", "w") as relatorio:
+        relatorio.write(f"Relatório de Arquivos\n")
+        relatorio.write(f"Gerado em: {data}\n\n")
+
+        for ext, qtd in resultado.items():
+            relatorio.write(f".{ext}: {qtd} arquivo(s)\n")
+
+    print("Relatório criado com sucesso.")
+
+def main():
+    print("Analisando arquivos da pasta...")
+    resultado = analisar_arquivos()
+    gerar_relatorio(resultado)
+
+if __name__ == "__main__":
+    main()
+
 

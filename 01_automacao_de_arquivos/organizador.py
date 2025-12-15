15beabb89fc961bff12deddc2f3c732d813c9816
babcaho <babcaho@gmail.com>
2025-12-15 16:11:50 -0300

    organizador.py

--- 01_automacao_de_arquivos/organizador.py
+++ 01_automacao_de_arquivos/organizador.py
@@ -1,7 +1,13 @@
 import os
 
-print("Programa iniciado")
-
 arquivos = os.listdir()
+extensoes = {}
 
-print("Arquivos encontrados:", arquivos)
+for arquivo in arquivos:
+    if "." in arquivo:
+        ext = arquivo.split(".")[-1]
+        extensoes[ext] = extensoes.get(ext, 0) + 1
+
+print("Resumo dos arquivos:")
+for ext, qtd in extensoes.items():
+    print(f".{ext}: {qtd} arquivo(s)")

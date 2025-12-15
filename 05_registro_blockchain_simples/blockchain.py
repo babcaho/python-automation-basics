31b59a5ae466533b1cbfc6dfe212274e7d7984cf
babcaho <babcaho@gmail.com>
2025-12-15 17:20:26 -0300

    blockchain.py

--- 05_registro_blockchain_simples/blockchain.py
+++ 05_registro_blockchain_simples/blockchain.py
@@ -7,19 +7,34 @@ class Bloco:
         self.timestamp = time.time()
         self.dados = dados
         self.hash_anterior = hash_anterior
-        self.hash = self.gerar_hash()
+        self.hash = self.calcular_hash()
 
-    def gerar_hash(self):
+    def calcular_hash(self):
         conteudo = f"{self.indice}{self.timestamp}{self.dados}{self.hash_anterior}"
         return hashlib.sha256(conteudo.encode()).hexdigest()
 
-blockchain = []
+def criar_blockchain():
+    blockchain = []
 
-bloco_genesis = Bloco(0, "Bloco Inicial", "0")
-blockchain.append(bloco_genesis)
+    bloco_genesis = Bloco(0, "Bloco Inicial", "0")
+    blockchain.append(bloco_genesis)
 
-bloco_1 = Bloco(1, "Transação A", bloco_genesis.hash)
-blockchain.append(bloco_1)
+    for i in range(1, 4):
+        novo_bloco = Bloco(i, f"Transação {i}", blockchain[-1].hash)
+        blockchain.append(novo_bloco)
 
-for bloco in blockchain:
-    print(bloco.indice, bloco.dados, bloco.hash)
+    return blockchain
+
+def main():
+    blockchain = criar_blockchain()
+
+    for bloco in blockchain:
+        print("Índice:", bloco.indice)
+        print("Dados:", bloco.dados)
+        print("Hash:", bloco.hash)
+        print("Hash anterior:", bloco.hash_anterior)
+        print("-" * 30)
+
+if __name__ == "__main__":
+    main()
+
